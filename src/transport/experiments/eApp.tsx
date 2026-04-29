9eef8de17789604d157370339552fe5115972f24
Valentin Khokhlov <on.valentin.khokhlov@gmail.com>
2026-04-29 12:09:28 -0400

    testing page reworked a bit for easier input to test the turn server

--- src/transport/experiments/eApp.tsx
+++ src/transport/experiments/eApp.tsx
@@ -6,12 +6,10 @@ import { Button } from "../../ui/elements";
 export default function EApp() {
 
 
-
     const ws = new WebSocket("wss://redacted");
     return (
         <Button onClick={() => {
             console.log("clicked");
         }}>Hello</Button>
-        
     )
 }
